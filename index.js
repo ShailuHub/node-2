183238864b0522fdfbae139f99962c62237bee78
Shailu <shailesh.respond@gmail.com>
2023-07-31 19:03:01 +0530

    fourth

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //app.use() -->creates middleware
-app.use((req, res, next) => {
-  console.log("I m first middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product"></input><input type="number" name="size"></input><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("I m next middleware");
-  res.send("<h1>Hello i am from Express Side</h1>");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
 });
 
 app.listen(3000, () => {
